8c0705ddd86a21f9cecbbbb7da97a4a38c47a4d3
“MocniPetar” <p.strbad0@gmail.com>
2022-12-15 11:35:05 +0100

    Terminal za sucelje

---
+++ 6. zadatak/Code/Liste.h
@@ -0,0 +1,38 @@
+#ifndef LISTE_H
+#define LISTE_H
+
+#include "PotrebniMaterijali.h"
+
+// LISTE
+
+struct Racun;
+typedef struct Racun *RacunPos;
+typedef struct Racun{
+    char Artikl[MAX_NUM_OF_CHAR];
+    int Kolicina;
+    float Cijena;
+    RacunPos Next;
+}racun;
+
+struct listaRacuna;
+typedef struct listaRacuna *RacuniPos;
+typedef struct listaRacuna{
+    char Racun_ID[MAX_NUM_OF_CHAR];
+    int Godina;
+    int Mjesec;
+    int Dan;
+    RacunPos Sadrzaj;
+    RacuniPos Next;
+}racuni;
+
+// FUNCKIJE
+
+int readFromFile(RacuniPos, char*);
+int readBillContent(RacuniPos, char*);
+int printBills(RacuniPos);
+int freeMemory(RacuniPos);
+int comparison(RacuniPos, RacuniPos);
+int moneySpent(RacuniPos, char*, int, int, int, int, int, int);
+RacunPos sortArticles(RacuniPos);
+
+#endif
